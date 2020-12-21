ff058afd20675772f4854bc39c5ad2a74975b9d4
crisoplo93 <cris_93_bep@hotmail.com>
2020-12-22 01:10:58 +0300

    thingys

--- sketch.js
+++ sketch.js
@@ -1,23 +1,70 @@
 let snowflakes = []; // array to hold snowflake objects
-let c_chill = color(153,0,153);
-let c_xmass = color(255,51,51);
+var mode = "Chill";
+var song;
+var songdir  = ["songs/you-wont-let-go.mp3", "songs/mariposa.mp3"];
+
 
 function windowResized()
 {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function musicXmass()
+{
+ mode = "Xmass";
+}
+
+function musicChill()
+{
+  if(typeof song !== "undefined")
+  {
+    if (song.isPlaying()) {
+      // .isPlaying() returns a boolean
+      song.stop();
+    } else {
+      song.play();
+    }
+  }
+  mode = "Chill";  
+  musicChillPlay();
+}
+
+function musicChillPlay()
+{
+ var i = int(random(2))
+
+ song = loadSound(songdir[i], loaded);
+ 
+}
+
+function loaded()
+{
+ song.play();
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index', '-1');
   fill(240);
   noStroke();
 }
 
 function draw() {
-  background(153,0,153);
+  let c_chill = color(153,0,153);
+  let c_xmass = color(169, 50, 38);
+  if (mode == "Xmass")
+  {
+    background(c_xmass);
+  }
+  else
+  {
+    background(c_chill);
+  }
+  
   let t = frameCount / 60; // update time
-  fill(255);
-  rect(0, 100, windowWidth, windowHeight-100);
+  fill(245, 238, 248);
+  rect(0, 100, windowWidth, windowHeight-200);
 
   // create a random number of snowflakes each frame
   for (let i = 0; i < random(5); i++) {
@@ -27,7 +74,15 @@ function draw() {
   // loop through snowflakes with a for..of loop
   for (let flake of snowflakes) {
     flake.update(t); // update snowflake position
-    flake.display(); // draw snowflake
+    if (mode == "Xmass")
+    {
+      flake.display(c_xmass); // draw snowflake
+    }
+    else
+    {
+      flake.display(c_chill); // draw snowflake
+    }
+    
   }
 }
 
@@ -59,7 +114,9 @@ function snowflake() {
     }
   };
 
-  this.display = function() {
+  this.display = function(colorS) {
+    if (this.posY>100 && this.posY< windowHeight-100) fill(colorS);
+    else fill (255);
     ellipse(this.posX, this.posY, this.size);
   };
 }
